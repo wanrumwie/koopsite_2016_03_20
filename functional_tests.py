aa915cf8837b9d6ee7fadaac6c8f4b7e5ca59bc6
adm koopsite <adm.koopsite@gmail.com>
2015-11-25 20:37:25 +0200

    Refactor lists views to work with request.POST

--- functional_tests.py
+++ functional_tests.py
@@ -1,3 +1,4 @@
+import time
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 import unittest
@@ -35,15 +36,30 @@ class NewVisitorTest(unittest.TestCase):
         # When she hits enter, the page updates, and now the page lists
         # "1: Buy peacock feathers" as an item in a to-do list
         inputbox.send_keys(Keys.ENTER)
+
         table = self.browser.find_element_by_id('id_list_table')
         rows = table.find_elements_by_tag_name('tr')
-        self.assertTrue(
-            any(row.text == '1: Buy peacock feathers' for row in rows)
-            )
+
+        time.sleep(10)
+        self.assertIn('1: Buy peacock feathers',
+                      [row.text for row in rows])
 
         # There is still a text box inviting her to add another item. She
-        # enters "Use peacock feathers to make a fly" (Edith is very methodical)
+        # enters "Use peacock feathers to make a fly" (Edith is very
+        # methodical)
+        inputbox = self.browser.find_element_by_id('id_new_item')
+        inputbox.send_keys('Use peacock feathers to make a fly')
+        inputbox.send_keys(Keys.ENTER)
+        # The page updates again, and now shows both items on her list
+        table = self.browser.find_element_by_id('id_list_table')
+        rows = table.find_elements_by_tag_name('tr')
+        self.assertIn('1: Buy peacock feathers', [row.text for row in rows])
+        self.assertIn('2: Use peacock feathers to make a fly' , [row.text for row in rows])
+        # Edith wonders whether the site will remember her list. Then she sees
+        # that the site has generated a unique URL for her -- there is some
+        # explanatory text to that effect.
         self.fail('Finish the test!')
+        # She visits that URL - her to-do list is still there.
 
         # The page updates again, and now shows both items on her list
 
@@ -59,3 +75,4 @@ class NewVisitorTest(unittest.TestCase):
 
 if __name__ == "__main__":
     unittest.main(warnings="ignore")
+    s = input('Press Enter to end: ')
