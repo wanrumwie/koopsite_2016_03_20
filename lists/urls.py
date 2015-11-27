0000699365bec4811eebf50ec446f0a0e4ae54bf
adm koopsite <adm.koopsite@gmail.com>
2015-11-27 11:05:57 +0200

    new URL + view for adding to existing lists. FT passes :-)

--- lists/urls.py
+++ lists/urls.py
@@ -1,5 +1,5 @@
 from django.conf.urls import url
-from lists.views import home_page, view_list, new_list
+from lists.views import home_page, view_list, new_list, add_item
 
 """
 /lists/ - під'єднано у модулі koopsite.urls.py:
@@ -7,9 +7,10 @@ from lists.views import home_page, view_list, new_list
 """
 urlpatterns = [
     # url(r'^person/table/$',   PersonTableView.as_view(), name='person-table'),
-    url(r'^$',                            home_page, name='home'),
-    url(r'^the-only-list-in-the-world/$', view_list, name='view_list'),
-    url(r'^new$',                         new_list,  name='new_list'),
+    url(r'^$',                  home_page, name='home'),
+    url(r'^(\d+)/$',            view_list, name='view_list'),
+    url(r'^(\d+)/add_item$',    add_item,  name='add_item'),
+    url(r'^new$',               new_list,  name='new_list'),
 ]
 # urlpatterns += staticfiles_urlpatterns()
 
--- lists/views.py
+++ lists/views.py
@@ -5,16 +5,30 @@ from lists.models import Item, List
 
 def home_page(request):
     template_name = 'home.html'
+    all = List.objects.all()
+    print('home_page(request):-----------')
+    print('List.objects.all() =', all)
+    for l in all: print('list_ =', l)
     return render(request, template_name)
 
-def view_list(request):
-    items = Item.objects.all()
-    return render(request, 'list.html', {'items': items})
+def view_list(request, list_id):
+    list_ = List.objects.get(id=list_id)
+    items = Item.objects.filter(list=list_)
+    data = {
+            'list': list_,
+            'items': items,
+            }
+    return render(request, 'list.html', data)
 
 def new_list(request):
-    list_ = List().save()
-    # list_ = List.objects.create()
+    # list_ = List().save()
+    list_ = List.objects.create()
     item_text = request.POST.get('item_text', '')
     Item.objects.create(text=item_text, list=list_)
-    return redirect('/lists/the-only-list-in-the-world/')
+    return redirect('/lists/%d/' % (list_.id,))
 
+def add_item(request, list_id):
+    list_ = List.objects.get(id=list_id)
+    item_text = request.POST.get('item_text', '')
+    Item.objects.create(text=item_text, list=list_)
+    return redirect('/lists/%d/' % (list_.id,))
