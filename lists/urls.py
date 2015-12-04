f310c16f965f5cc9dfedc5b833629e61ed878063
adm koopsite <adm.koopsite@gmail.com>
2015-12-04 19:41:09 +0200

    Lists: enforce model validation in list view

--- lists/urls.py
+++ lists/urls.py
@@ -1,5 +1,5 @@
 from django.conf.urls import url
-from lists.views import home_page, view_list, new_list, add_item
+from lists.views import home_page, view_list, new_list
 
 """
 /lists/ - під'єднано у модулі koopsite.urls.py:
@@ -9,7 +9,6 @@ urlpatterns = [
     # url(r'^person/table/$',   PersonTableView.as_view(), name='person-table'),
     url(r'^$',                  home_page, name='home'),
     url(r'^(\d+)/$',            view_list, name='view_list'),
-    url(r'^(\d+)/add_item$',    add_item,  name='add_item'),
     url(r'^new$',               new_list,  name='new_list'),
 ]
 # urlpatterns += staticfiles_urlpatterns()
--- lists/views.py
+++ lists/views.py
@@ -10,12 +10,17 @@ def home_page(request):
 
 def view_list(request, list_id):
     list_ = List.objects.get(id=list_id)
-    items = Item.objects.filter(list=list_)
-    data = {
-            'list': list_,
-            'items': items,
-            }
-    return render(request, 'list.html', data)
+    error = None
+    if request.method == 'POST':
+        try:
+            item = Item(text=request.POST['item_text'], list=list_)
+            item.full_clean()
+            item.save()
+            return redirect('/lists/%d/' % (list_.id,))
+        except ValidationError:
+            error = "You can't have an empty list item"
+    return render(request, 'list.html', {'list': list_, 'error': error})
+
 
 def new_list(request):
     list_ = List.objects.create()
@@ -30,8 +35,3 @@ def new_list(request):
         return render(request, 'home.html', {"error": error})
     return redirect('/lists/%d/' % (list_.id,))
 
-def add_item(request, list_id):
-    list_ = List.objects.get(id=list_id)
-    item_text = request.POST.get('item_text', '')
-    Item.objects.create(text=item_text, list=list_)
-    return redirect('/lists/%d/' % (list_.id,))
